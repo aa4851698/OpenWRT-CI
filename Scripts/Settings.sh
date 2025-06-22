04f12bd84f77403137d61271e9f547c1de8f61fb
aa4851698 <sgwapk@163.com>
2025-06-22 11:24:38 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -3,7 +3,7 @@
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
-sed -i 's/192.168.31.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.31.1/192.168.1.1/g' $GITHUB_WORKSPACE/wrt/package/base-files/files/bin/config_generate
 #添加编译日期标识
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 
